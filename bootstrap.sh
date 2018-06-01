0ea3cb7c132877f199c142004b27fcfb0f0445be
Valentin Mihaylov <valentin.mihaylov@gmail.com>
2018-06-01 17:16:38 +0200

    using apt-get, not apt

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt install -y git ansible
+sudo apt-get install -y git ansible
 # git clone https://github.com/jdauphant/ansible-ubuntu-desktop.git
 # ansible-playbook installation.yml --sudo -K -c local -i "localhost,"
 
