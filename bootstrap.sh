8070876466756646778ae1e63ddad6d72fe69a43
Valentin Mihaylov <valentin.mihaylov@gmail.com>
2018-06-09 15:33:26 +0200

    apt upgrade added

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ DEVOPS_REPO_NAME="devops"
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -y git ansible
 
 git config --global user.email "valentin.mihaylov@gmail.com"
