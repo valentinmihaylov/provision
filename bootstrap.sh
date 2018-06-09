38064bf647b37abcb7b7903ad60f4bc9af45a164
Valentin Mihaylov <valentin.mihaylov@gmail.com>
2018-06-09 16:26:55 +0200

    trying to install ansible >= 2.5

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,12 @@
 
 DEVOPS_REPO_NAME="devops"
 
+sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get install software-properties-common
+sudo apt-get remove ansible -y
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get upgrade
 sudo apt-get install -y git ansible
 
 git config --global user.email "valentin.mihaylov@gmail.com"
