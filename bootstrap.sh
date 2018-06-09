5ddf33dc5741d15b768190e6eb4a4d4de7483d02
Valentin Mihaylov <valentin.mihaylov@gmail.com>
2018-06-09 17:23:00 +0200

    apt update after adding ansible repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ DEVOPS_REPO_NAME="devops"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
 sudo apt-get install software-properties-common git ansible -y
 
 git config --global user.email "valentin.mihaylov@gmail.com"
