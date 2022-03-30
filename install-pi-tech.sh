7d650d172f10bdfe9ecd434686b712104c6f7579
valentinmihaylov <valentin.mihaylov@gmail.com>
2022-03-30 11:36:52 +0200

    logging

--- install-pi-tech.sh
+++ install-pi-tech.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+echo -e "\n###\nupdate system, install git and ansible\n###\n"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install software-properties-common git ansible -y
 
+echo -e "\n###\nconfigure git\n###\n"
 git config --global user.email "valentin.mihaylov@gmail.com"
 git config --global user.name "Valentin Mihaylov"
 
+echo -e "\n###\nget repo from bitbucket\n###\n"
 git clone https://mihaylov-software@bitbucket.org/mihaylov-software/devops.git
 
+echo -e "\n###\nstart provisioning\n###\n"
 cd devops/raspi
 ./install-pi-tech.sh
