874a039f78350c1be4ece93c2b56c3d21a8d0751
valentinmihaylov <valentin.mihaylov@gmail.com>
2022-03-30 11:33:46 +0200

    logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,18 @@
 
 DEVOPS_REPO_NAME="devops"
 
+echo -e "\n###\nupdate system, install git and ansible\n###\n"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install software-properties-common git ansible -y
 
+echo -e "\n###\nconfigure git\n###\n"
 git config --global user.email "valentin.mihaylov@gmail.com"
 git config --global user.name "Valentin Mihaylov"
 
+echo -e "\n###\nget repo from bitbucket\n###\n"
 if [ ! -d "$DEVOPS_REPO_NAME" ]; then
     git clone https://mihaylov-software@bitbucket.org/mihaylov-software/devops.git
 else
@@ -21,5 +24,6 @@ fi
 
 sudo apt-get remove git -y # git will probably have an old version
 
+echo -e "\n###\nstart provisioning\n###\n"
 cd $DEVOPS_REPO_NAME/provision
 ./install-laptop.sh
