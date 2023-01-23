3c4d3294d52242e01052a421e8a45864fbf4dc65
valentinmihaylov <valentin.mihaylov@gmail.com>
2023-01-24 00:37:33 +0100

    error handling for new pc installation

--- scripts/prepare-repo-access.sh
+++ scripts/prepare-repo-access.sh
@@ -3,10 +3,13 @@
 # $1 like user@pc.local
 # $2 like /path/to/bitbucket-pass-file
 
+sudo mkdir -p /install/
+sudo chmod 777 /install/
 scp $1:$2 $2
 scp $1:~/.ssh/id_rsa ~/.ssh/id_rsa
 
 echo -e "\n###\nconfigure git\n###\n"
 
+sudo apt install git
 git config --global user.email "valentin.mihaylov@gmail.com"
 git config --global user.name "Valentin Mihaylov"
